2c0a32ceb402e25e2599aa92eed7c1a6ecf46c3e
jackshang <jack.shang@grapecity.com>
2018-05-18 14:32:49 +0800

    fix crash issue on IE

--- AspNetCoreDemo/AspNetCore+Angular2/ClientApp/boot-client.ts
+++ AspNetCoreDemo/AspNetCore+Angular2/ClientApp/boot-client.ts
@@ -1,5 +1,21 @@
 import 'reflect-metadata';
 import 'zone.js';
+/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+
+/***********added*************/
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module.client';
